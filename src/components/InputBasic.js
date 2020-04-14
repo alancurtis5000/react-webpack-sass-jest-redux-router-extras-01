881e91d32c502f943b5572e371d0b172827f279c
Alan Curtis <alancurtis5000@gmail.com>
2020-04-14 16:47:42 -0600

    added InputBasic component.

---
+++ src/components/InputBasic.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Proptypes from 'prop-types'
+
+const InputBasic = (props) => (
+  <div className="InputBasic">
+    <label htmlFor={props.id}>{props.label}</label>
+    <input 
+      type={props.type} 
+      id={props.id} 
+      onChange={props.handleOnChange} 
+      placeholder={props.placeholder}
+    />
+    {props.errors.map((error, i)=>(<div key={i} className="error" >{error}</div>))}
+  </div>
+)
+
+InputBasic.defaultProps = {
+  value:"",
+  label:"",
+  type:"text",
+  id:"unique",
+  placeholder:"",
+  handleOnChange:(e)=>{console.log("handleOnChange", e.target.value)},
+  errors:["to many characters", "Missing @ symbol"]
+}
+
+InputBasic.propTypes = {
+  value: Proptypes.any,
+  label: Proptypes.string,
+  type: Proptypes.string,
+  id: Proptypes.string,
+  placeholder: Proptypes.string,
+  handleOnChange: Proptypes.func,
+  errors: Proptypes.array
+}
+
+export default InputBasic;
