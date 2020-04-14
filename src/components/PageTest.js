e3394c6c2e25913040b4a3d15e5baf107ef5de7e
Alan Curtis <alancurtis5000@gmail.com>
2020-04-14 13:06:59 -0600

    added inputs to PageTest

--- src/components/PageTest.js
+++ src/components/PageTest.js
@@ -26,6 +26,37 @@ class PageTest extends Component {
         <h1>React PageTest!</h1>
         <button onClick={this.handleOnClick}>test click</button>
         <button onClick={this.logProps}>log props</button>
+
+        <div style={{display:"flex", justifyContent:"center", flexDirection:'column', width:'300px', margin:"auto"}}>
+            
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" className="firstName" id="firstName"/>
+
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" className="lastName" id="lastName"/>
+
+          <label htmlFor="email"> Email </label>
+          <input type="email" className="email" id="email"/>
+
+          <label htmlFor="phone">Phone</label>
+          <input type="tel" className="phone" id="phone"/>
+
+          <label htmlFor="comment">Comment</label>
+          <textarea className="comment" id="comment" cols="30" rows="10"></textarea>
+
+          <label htmlFor="age">Age</label>
+          <input type="number" className="age" id="age" min="1" max="130"/>
+
+          <label htmlFor="startDate">Start Date</label>
+          <input type="date" id="startDate"
+            value="2018-07-22"
+            min="2018-01-01" 
+            max="2018-12-31">
+          </input>
+          
+          <div>drop down</div>
+          <div> input with type ahead and drop down</div>
+        </div>
       </div>
     )
   }
