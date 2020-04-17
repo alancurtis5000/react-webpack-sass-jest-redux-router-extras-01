ee55e9d1880a02e95b5340c4698fa84b4bbad961
Alan Curtis <alancurtis5000@gmail.com>
2020-04-17 11:31:05 -0600

    removed unused code.

--- src/helpers/inputValidator.js
+++ src/helpers/inputValidator.js
@@ -8,38 +8,16 @@ export const isEmail = ( email ) => {
   const errors = [];
   // validate email
   if ( email === "" ) {
-    if (!errors.includes(errorRequiredField)){
-      errors.push(errorRequiredField);
-    }
+    errors.push(errorRequiredField);
     return errors;
-  } else {
-    let index = errors.indexOf(errorRequiredField);
-    if (index !== -1){
-      errors.splice(index, 1);
-    }
-  }
+  } 
   if ( !email.includes("@") ) {
-    if (!errors.includes(errorRequiredSymbolAt)){
-      errors.push(errorRequiredSymbolAt);
-    }
-  } else {
-    let index = errors.indexOf(errorRequiredSymbolAt);
-    if (index !== -1){
-      errors.splice(index, 1);
-    }
-  }
+    errors.push(errorRequiredSymbolAt);
+  } 
   if ( !email.includes(".") ) {
-    if (!errors.includes(errorRequiredSymbolPeriod)){
-      errors.push(errorRequiredSymbolPeriod);
-    }
-  } else {
-    let index = errors.indexOf(errorRequiredSymbolPeriod);
-    if (index !== -1){
-      errors.splice(index, 1);
-    }
-  }
+    errors.push(errorRequiredSymbolPeriod);
+  } 
   return errors;
-  
 }
 
 export const isRequired = ( value ) => {
