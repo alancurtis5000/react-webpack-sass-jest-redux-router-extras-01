c3f88da1aa1f29edc0963c67b7d902b35c5cec37
Alan Curtis <alancurtis5000@gmail.com>
2020-04-27 18:34:55 -0600

    one way to connect to a database.l

--- server/server.js
+++ server/server.js
@@ -3,12 +3,35 @@ const express= require('express');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 9090;
+const { Client } = require('pg');
 
 // Destructure env file.
 // const {SERVER_PORT} = process.env;
+const {
+  DATABASE_USER ,
+  DATABASE_PASSWORD ,
+  DATABASE_HOST ,
+  DATABASE_PORT ,
+  DATABASE_NAME 
+} = process.env
+console.log( {DATABASE_USER})
 
 // Connect to Database.
-// use massive
+
+const client = new Client({
+  user: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  database: DATABASE_NAME,
+});
+
+client.connect()
+.then(() => console.log("connected -----------"))
+.then(() => client.query("select * from users"))
+.then((results) => console.log("results from query", {r:results.rows}))
+.catch(e=> console.log("error ----------", e))
+.finally(()=>client.end());
 
 // Middleware import.
 const bodyParser= require('body-parser');
