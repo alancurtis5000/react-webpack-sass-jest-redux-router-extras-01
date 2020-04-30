ec3ddec3e523224e81f96b88c0c391a6f79c1c84
Alan Curtis <alancurtis5000@gmail.com>
2020-04-30 12:56:08 -0600

    made axios test to return passing.

--- src/tests/__mocks__/axios.js
+++ src/tests/__mocks__/axios.js
@@ -1,6 +1,6 @@
 export const get = url =>{
   console.log('mock ---------------- axios get')
-  return Promise.resolve({data:{title:"hello charlie"}})
+  return Promise.resolve({data:{title:"delectus aut autem"}})
 }
 
 export const post = (url, body) =>{
